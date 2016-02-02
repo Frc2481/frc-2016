c564b5644f6113c34bef9ff68968c0533a34c69d
Ampithos <thomas.speciale98@gmail.com>
2016-02-01 19:02:04 -0600

    Changed shooter to closedloop control on a cantalon

--- r2016/src/Commands/TurnOnShooterCommand.h
+++ r2016/src/Commands/TurnOnShooterCommand.h
@@ -6,21 +6,30 @@
 
 class TurnOnShooterCommand: public CommandBase
 {
+private:
+	double m_shooterSpd;
 public:
-	TurnOnShooterCommand() : CommandBase("TurnOnShooterCommand"){
-		Requires(shooter.get());
+	TurnOnShooterCommand(double spd = -1) : CommandBase("TurnOnShooterCommand"){
+		Requires(CommandBase::shooter.get());
+		if (spd == -1){
+			m_shooterSpd = SmartDashboard::GetNumber("Shooter Speed", 3300);
+		}
+		else {
+			m_shooterSpd = spd;
+		}
 	}
 	void Initialize(){
-		shooter->TurnOn();
+		shooter->SetShooterSpeed(m_shooterSpd);
 	}
-	void Execute(){
-	}
+	void Execute(){}
 	bool IsFinished(){
-		return true;
+		return false;
 	}
 	void End(){
+		shooter->TurnOff();
 	}
 	void Interrupted(){
+		End();
 	}
 };
 
