b262d45de908e1732d7e020c73975d8e5fbbdfeb
Ampithos <thomas.speciale98@gmail.com>
2016-02-05 17:12:50 -0600

    created RotateToAngleFromCameraCommand located inside the RotateToAngleCommand

--- r2016/src/Commands/RotateToAngleCommand.h
+++ r2016/src/Commands/RotateToAngleCommand.h
@@ -7,24 +7,15 @@
 
 class RotateToAngleCommand: public PIDCommand
 {
-private:
+protected:
 	double m_angle;
 	int m_onTarget;
 public:
-	RotateToAngleCommand(double angle, bool useCamera = false)
+	RotateToAngleCommand(double angle)
 			: PIDCommand("RotateToAngle", .020, .002, 0){
-
 		Requires(CommandBase::driveTrain.get());
-
-		if (useCamera == false) {
-			m_angle = angle;
-		}
-		else {
-			CommandBase::mCameraProcessor->calculate();
-			m_angle = CommandBase::mCameraProcessor->getAngle();
-		}
+		m_angle = angle;
 		m_onTarget = 0;
-		SetSetpoint(CommandBase::driveTrain->GetIMU()->GetAngle() + m_angle);
 		SmartDashboard::PutNumber("Rotation I Zone", 10);
 		SmartDashboard::PutNumber("DriveTrain Rotate I", .002);
 		SmartDashboard::PutNumber("DriveTrain Rotate P", .020);
@@ -40,31 +31,35 @@ public:
 	}
 
 
-	void Initialize(){
+	virtual void Initialize(){
 		GetPIDController()->Reset();
 		GetPIDController()->Enable();
 		m_onTarget = 0;
+
+		double setpoint = CommandBase::driveTrain->GetIMU()->GetAngle() + m_angle;
+		setpoint = RoboUtils::constrainDegNeg180To180(setpoint);
+		SetSetpoint(setpoint);
 	}
 
 	void Execute(){
-			double curError = fabs(GetSetpoint()-GetPosition());
-			double P = SmartDashboard::GetNumber("DriveTrain Rotate P", 0);
+		double curError = fabs(GetSetpoint ()-GetPosition());
+		SmartDashboard::PutNumber("Rotate Angle", curError);
+		double P = SmartDashboard::GetNumber("DriveTrain Rotate P", 0);
 
-			std::shared_ptr<PIDController> pid = GetPIDController();
-			if (curError < SmartDashboard::GetNumber("Rotation I Zone", 10)) {
-				double I = SmartDashboard::GetNumber("DriveTrain Rotate I", 0);
-				pid->SetPID(P, I, pid->GetD());
-			}
-			else {
-				pid->SetPID(P, 0, pid->GetD());
-			}
-
-			if (curError < .5) {
-				m_onTarget++;
-			}
-			else {
-				m_onTarget = 0;
-			}
+		std::shared_ptr<PIDController> pid = GetPIDController();
+		if (curError < SmartDashboard::GetNumber("Rotation I Zone", 10)) {
+			double I = SmartDashboard::GetNumber("DriveTrain Rotate I", 0);
+			pid->SetPID(P, I, pid->GetD());
+		}
+		else {
+			pid->SetPID(P, 0, pid->GetD());
+		}
+		if (curError < .5) {
+			m_onTarget++;
+		}
+		else {
+			m_onTarget = 0;
+		}
 	}
 
 	bool IsFinished(){
@@ -76,4 +71,15 @@ public:
 	void Interrupted(){}
 };
 
+class RotateToAngleFromCameraCommand : public RotateToAngleCommand {
+public:
+	RotateToAngleFromCameraCommand() : RotateToAngleCommand(0) {
+	}
+
+	void Initialize(){
+		double scale = 19.9 / 15.45; //TODO: Figure out why the camera or gyro angle is wrong.
+		m_angle = CommandBase::mCameraProcessor->getAngle() * scale;
+		RotateToAngleCommand::Initialize();
+	}
+};
 #endif
