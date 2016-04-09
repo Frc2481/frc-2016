e4cbf5ee6b826d456ca8d64a2504c1bf6c952616
2481-Programmer <2481>
2016-04-09 12:21:34 -0500

    Added raising shooter before aligning to target.

--- r2016/src/Commands/CameraRotateCommandGroup.h
+++ r2016/src/Commands/CameraRotateCommandGroup.h
@@ -4,12 +4,14 @@
 #include "Commands/CommandGroup.h"
 #include "Commands/LockOnTargetCommand.h"
 #include "Commands/WaitForCameraSteadyCommand.h"
+#include "Commands/ShooterUpCommand.h"
 #include "WPILib.h"
 
 class CameraRotateCommandGroup: public CommandGroup
 {
 public:
 	CameraRotateCommandGroup(CameraProcessor::target_type_t target){
+			AddSequential(new ShooterUpCommand());
 			AddSequential(new LockOnTargetCommand(target));
 			AddSequential(new WaitForCameraSteadyCommand());
 			AddSequential(new RotateToAngleFromCameraCommand());
